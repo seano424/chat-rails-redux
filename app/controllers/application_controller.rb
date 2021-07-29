cd243a1bc00224ef2d88bbbe96486b3b55d60fd5
Sean OReilly <soreilly424@gmail.com>
2021-07-28 19:54:23 -0500

    styling added to authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  layout :layout_by_resource
+
+  def layout_by_resource
+    user_signed_in? ? "application" : "authentication"
+  end
+
 end
